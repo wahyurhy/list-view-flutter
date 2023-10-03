092896f8716edbcce63b1c77427d82625febea7d
wahyurhy <wahyurahayu82@gmail.com>
2023-10-03 08:23:32 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'List View',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'List View Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<int> numberList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
+        ),
+        body: ListView(
+          children: numberList.map((number) {
+            return Container(
+              height: 250,
+              decoration: BoxDecoration(
+                  color: Colors.grey, border: Border.all(color: Colors.black)),
+              child: Center(
+                child: Text(
+                  '$number',
+                  style: const TextStyle(fontSize: 50),
+                ),
+              ),
+            );
+          }).toList(),
+        )
+    );
+  }
+}
